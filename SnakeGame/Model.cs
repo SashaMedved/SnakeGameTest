05d04964abb8f93689e6d4bea624341b1bbbaf75
KuromiVoid1007 <coolvip1007@mail.ru>
2024-04-02 03:04:05 +0500

    model_controls

--- SnakeGame/Model.cs
+++ SnakeGame/Model.cs
@@ -8,6 +8,17 @@ namespace SnakeGame
 {
     internal class Model
     {
+        public Form1 form1;
+        public PictureBox snake;
+        public int dirX = 1;
+        public int dirY = 0;
+
+        public Model(Form1 form1)
+        {
+            this.form1 = form1;
+            snake = form1.Controls["snake"] as PictureBox;
+        }
+
         public void EatFood()
         {
 

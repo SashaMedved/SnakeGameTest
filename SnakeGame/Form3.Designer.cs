cde745196aa5b7cb5d26edb1b9ba94db76125c4b
SashaMedved <laskov98rus@list.ru>
2024-04-06 22:16:51 +0500

    FirstMenu

--- SnakeGame/Form3.Designer.cs
+++ SnakeGame/Form3.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SnakeGame
+﻿using System.Windows.Forms;
+
+namespace SnakeGame
 {
     partial class Form3
     {
@@ -30,27 +32,55 @@
         {
             buttonContinue = new Button();
             LabelTextScreen = new Label();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // buttonContinue
             // 
-            buttonContinue.Location = new Point(177, 517);
+            buttonContinue.BackColor = Color.Transparent;
+            buttonContinue.FlatAppearance.BorderSize = 0;
+            buttonContinue.FlatStyle = FlatStyle.Flat;
+            buttonContinue.ForeColor = Color.IndianRed;
+            buttonContinue.Location = new Point(-9, 388);
+            buttonContinue.Margin = new Padding(0);
             buttonContinue.Name = "buttonContinue";
-            buttonContinue.Size = new Size(120, 63);
-            buttonContinue.TabIndex = 0;
-            buttonContinue.Text = "continue";
-            buttonContinue.UseVisualStyleBackColor = true;
+            buttonContinue.Size = new Size(526, 164);
+            buttonContinue.TabIndex = 12;
+            buttonContinue.Text = "Start";
+            buttonContinue.UseVisualStyleBackColor = false;
             buttonContinue.Click += buttonContinue_Click;
             // 
             // LabelTextScreen
             // 
             LabelTextScreen.AutoSize = true;
-            LabelTextScreen.BackColor = SystemColors.ActiveBorder;
-            LabelTextScreen.Location = new Point(123, 71);
+            LabelTextScreen.BackColor = Color.Transparent;
+            LabelTextScreen.Location = new Point(123, 86);
             LabelTextScreen.Name = "LabelTextScreen";
             LabelTextScreen.Size = new Size(254, 15);
             LabelTextScreen.TabIndex = 1;
             LabelTextScreen.Text = "Для управления змейкой используйте WASD";
+            LabelTextScreen.Click += LabelTextScreen_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(158, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 15);
+            label1.TabIndex = 13;
+            label1.Text = "Приветствуем вас в SnakeGame!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Location = new Point(111, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(294, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Для начала игры нажмите START на главном экране";
             // 
             // Form3
             // 
@@ -59,8 +89,10 @@
             BackColor = SystemColors.ControlDarkDark;
             BackgroundImageLayout = ImageLayout.None;
             ClientSize = new Size(502, 707);
-            Controls.Add(LabelTextScreen);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(buttonContinue);
+            Controls.Add(LabelTextScreen);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form3";
             Opacity = 0.9D;
@@ -73,8 +105,9 @@
         }
 
         #endregion
-
-        private Button buttonContinue;
         private Label LabelTextScreen;
+        private Button buttonContinue;
+        private Label label1;
+        private Label label2;
     }
 }
--- SnakeGame/Form3.cs
+++ SnakeGame/Form3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,31 @@ namespace SnakeGame
     public partial class Form3 : Form
     {
         private Form1 form1;
+        public Font guno;
         public Form3(Form1 form1)
         {
             InitializeComponent();
             this.form1 = form1;
+            LoadFont();
+            buttonContinue.Font = guno;
         }
 
         private void buttonContinue_Click(object sender, EventArgs e)
         {
             form1.Enabled = true;
             this.Close();
+        }
+
+        private void LoadFont()
+        {
+            PrivateFontCollection custom_font = new PrivateFontCollection();
+            custom_font.AddFontFile("guno.otf");
+            guno = new Font(custom_font.Families[0], 30);
+        }
+
+        private void LabelTextScreen_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
